ef4c1cb35e99f1787861e19bc1a9af82a4818baf
Julia raguillet <48208434+DWEntertainement@users.noreply.github.com>
2022-12-30 16:56:25 +0100

    Ajoutez des fichiers projet.

---
+++ ExDW Navigateur 2.0/Favoris.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExDW_Navigateur_2._0
+{
+    /// <summary>
+    /// Logique d'interaction pour Favoris.xaml
+    /// </summary>
+    public partial class Favoris : Page
+    {
+        public Favoris()
+        {
+            InitializeComponent();
+        }
+
+        private void Google_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+        private void DW_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ ExDW Navigateur 2.0/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExDW_Navigateur_2._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Favoris_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new Favoris();
+        }
+        private void Rechercher_Click(object sender, RoutedEventArgs e)
+        {
+            string url = urlTxt.Text;
+            //TODO: Vérifie si c'est une URL Ok..
+            try
+            {
+                navigateur.Navigate(url);
+            }
+            catch
+            {
+                Console.WriteLine("Erreur");
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (navigateur.CanGoBack)
+            {
+                navigateur.GoBack();
+            }
+            
+        }
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            if (navigateur.CanGoForward)
+            {
+                navigateur.GoForward();
+            }
+            
+        }
+
+        private void Nouveau_Code_Click(object sender, RoutedEventArgs e)
+        {
+            AdminCode fen = new AdminCode();
+            fen.Show();
+        }
+    }
+}
